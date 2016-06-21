2f035e78b106afdd59897c70235c9355713f19b3
张世通 <zhangst0720@163.com>
2016-06-21 17:39:04 +0800

    删除APP

--- app/src/androidTest/java/com/okq/rs485/ExampleInstrumentationTest.java
+++
@@ -1,9 +0,0 @@
-package com.okq.rs485;
-
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleInstrumentationTest {
-}
